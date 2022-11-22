c58d7cec40d5d9e39008d65e678505099c3dbb27
Dmytro Soroka <dimasoroka17@gmail.com>
2022-11-22 22:53:05 +0200

    Update Requests.js

--- src/api/Requests.js
+++ src/api/Requests.js
@@ -1,39 +1,34 @@
 import { KEY, ORIGIN } from '../constants/credential';
 
-function fetchDataWeek() {
-  return fetch(`${ORIGIN}trending/movie/week?api_key=${KEY}`).then(response => {
-    return response.json();
-  });
+async function fetchDataWeek() {
+  const response = await fetch(`${ORIGIN}trending/movie/week?api_key=${KEY}`);
+  return await response.json();
 }
 const apiWeekMovies = { fetchDataWeek };
 export default apiWeekMovies;
 
-export const fetchMovie = movieId => {
-  return fetch(`${ORIGIN}movie/${movieId}?api_key=${KEY}`).then(response => {
-    return response.json();
-  });
+export const fetchMovie = async movieId => {
+  const response = await fetch(`${ORIGIN}movie/${movieId}?api_key=${KEY}`);
+  return await response.json();
 };
 
-export const castMovie = movieId => {
-  return fetch(`${ORIGIN}movie/${movieId}/credits?api_key=${KEY}`).then(
-    response => {
-      return response.json();
-    }
+export const castMovie = async movieId => {
+  const response = await fetch(
+    `${ORIGIN}movie/${movieId}/credits?api_key=${KEY}`
   );
+  return await response.json();
 };
 
-export const reviewsMovie = movieId => {
-  return fetch(`${ORIGIN}movie/${movieId}/reviews?api_key=${KEY}`).then(
-    response => {
-      return response.json();
-    }
+export const reviewsMovie = async movieId => {
+  const response = await fetch(
+    `${ORIGIN}movie/${movieId}/reviews?api_key=${KEY}`
   );
+  return await response.json();
 };
 
-export const searchMovie = search => {
-  return fetch(`${ORIGIN}search/movie?api_key=${KEY}&query=${search}`).then(
-    response => {
-      return response.json();
-    }
+export const searchMovie = async search => {
+  const response = await fetch(
+    `${ORIGIN}search/movie?api_key=${KEY}&query=${search}`
   );
+  return await response.json();
 };
